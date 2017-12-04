f969af44349ce66a4350585624cd7d7cdfeae5de
kainagel <nagel@vsp.tu-berlin.de>
2017-12-04 13:46:44 +1100

    reorganize data directory

---
+++ data/osm/2017-11-01-by-dhirendra/extract-osm.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/env bash
+
+#
+# Example script for extracting rectangular bounds from Australia OSM
+# Author: dsingh
+#
+
+
+#DIR=`dirname "$0"`
+#cd $DIR
+
+# download the latest OSM extract for Australia from:
+# http://download.gisgraphy.com/openstreetmap/pbf/AU.tar.bz2
+
+PBF_ARCHIVE=AU.tar.bz2
+printf "\nDownloading the latest OSM extract for Australia...\n\n"
+wget -c -N http://download.gisgraphy.com/openstreetmap/pbf/$PBF_ARCHIVE
+
+
+printf "\nExtracting PBF from archive...\n\n"
+tar -jxvf tmp.tar.bz2
+
+# Mount Alexander Shire bounding box
+top=-36.7323
+left=144.0294
+bottom=-37.2686
+right=144.4888
+
+printf "\nExtracting Mount Alexander Shire region (bounding box [$top,$right $bottom,$left]) ... \n\n"
+osmosis  --read-pbf file=AU  \
+    --bounding-box top=$top left=$left bottom=$bottom right=$right \
+    --write-xml file=- | bzip2 > mount-alexander-shire.osm.bz2
+
+#osmosis --read-xml city.osm --tf accept-ways highway=* --used-node --write-xml highways.osm
+
+printf "\nExtracting Mount Alexander Shire roads ... \n\n"
+bzcat mount-alexander-shire.osm.bz2 | osmosis  --read-xml enableDateParsing=no file=- --tf accept-ways highway=* --used-node --write-xml file=- | bzip2 > mount-alexander-shire-roads.osm.bz2
+
+
+#cd -
---
+++ data/vista/2017-11-01-vista2009/README.md
@@ -0,0 +1,30 @@
+# VISTA directory
+
+* [About this directory](#about-this-directory)
+* [Files Contained](#files-contained)
+* [Contributors](#contributors)
+
+
+## About this directory
+
+
+## Files contained
+
+The following list describes the files contained in the directory
+
+* `VISTA_v3_Online_Data_CSV_2009.zip`. Provided by Jonathan Arundel; he in turn got it from some http address.
+
+* `Persons_VISTA09_v3_VISTA_Online.csv`. Is contained in the above zipfile; extracted here so that code can run on the repository 'as is'.
+
+* `Trips_VISTA09_v3_VISTA_Online.csv`.  Is contained in the above zipfile; extracted here so that code can run on the repository 'as is'.
+
+* `vista09-documentation-v10.pdf`.  Provided by Kai Nagel; he in turn found it on some website.
+
+## Contributors
+
+* See above.
+* Documentation: Karthikey Surineni, RMIT University
+
+
+
+
